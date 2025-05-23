dbbb271f5ca9a0c42f3d82462f4b05995efeb170
Māris Suss <marissuss@gmail.com>
2025-05-23 09:57:50 +0300

    deploy pain

--- backend/index.php
+++ backend/index.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+if (php_sapi_name() === 'cli-server') {
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    if ($path !== '/' && file_exists(__DIR__ . $path)) {
+        return false;
+    }
+}
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
