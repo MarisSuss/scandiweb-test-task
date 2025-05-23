ab6b6a36ce32c177b142dd15ec625cf9094186db
Māris Suss <marissuss@gmail.com>
2025-05-23 09:23:55 +0300

    index moved to root in backend

--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
@@ -11,9 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
 $dotenv->load();
 
-require_once __DIR__ . '/../src/routes/web.php';
+require_once __DIR__ . '/src/routes/web.php';
